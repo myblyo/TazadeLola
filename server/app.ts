ebfe334b781aad9a9c7492ae6eda228fe19f1a85
Myls Blanza <mylsblanza290@gmail.com>
2026-05-18 15:01:26 +0200

    refractor

--- server/app.ts
+++ server/app.ts
@@ -1,24 +1,23 @@
 import express from 'express'
 import cors from 'cors'
 import menuRoutes from './routes/menu.routes.ts'
- 
+
 const app = express()
 const PORT = 3000
- 
+
 app.use(cors())
 app.use(express.json())
- 
+
 // Rutas
 app.use('/api/menu', menuRoutes)
- 
+
 // Health check
 app.get('/', (_req, res) => {
   res.json({ ok: true, message: 'Taza de Lola API funcionando ☕' })
 })
- 
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })
- 
+
 export default app
- 
